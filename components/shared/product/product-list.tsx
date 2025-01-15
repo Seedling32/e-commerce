4c25a38386709466e38736d7f00aa1a38c903494
Seedling <david.r.graham32@gmail.com>
2025-01-15 10:45:13 -0500

    Create product card create product-card in shared product. impord card from shadcn ui

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from "./product-card";
+
 const ProductList = ({data, title, limit}: {data: any; title?: string; limit?: number }) => {
   const limitedData = limit ? data.slice(0, limit) : data;
   return ( 
@@ -8,7 +10,10 @@ const ProductList = ({data, title, limit}: {data: any; title?: string; limit?: n
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {limitedData.map((product: any) => (
-            <div key={product.name}>{product.name}</div>
+            <ProductCard
+            key={product.slug}
+            product={product}
+            />
           ))}
         </div>
       ) : (
