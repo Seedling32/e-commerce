af9d62892cf3c7d5bac8a76295cd0266f6abd871
Seedling <david.r.graham32@gmail.com>
2025-01-15 10:17:33 -0500

    Create product list Create product folder in shared/ and product-list Bring in sample data as temp db.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const Homepage = () => {
+  
   return ( 
-    <>Prostore</>
+    <>
+    <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+    </>
    );
 }
  
