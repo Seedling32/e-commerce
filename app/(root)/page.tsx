e86b9d686e76dbe5380eb9750464c299af9fcf86
Seedling <david.r.graham32@gmail.com>
2025-01-15 17:24:31 -0500

    install prisma and initialize. Format all files create prettier and eslint files for formatting.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,12 @@
-import sampleData from "@/db/sample-data";
-import ProductList from "@/components/shared/product/product-list";
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
 const Homepage = () => {
-  
-  return ( 
+  return (
     <>
-    <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
     </>
-   );
-}
- 
+  );
+};
+
 export default Homepage;
--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,17 +1,24 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuContent, DropdownMenuCheckboxItem } from "@/components/ui/dropdown-menu";
-import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon, SunMoon  } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem,
+} from '@/components/ui/dropdown-menu';
+import { useTheme } from 'next-themes';
+import { SunIcon, MoonIcon, SunMoon } from 'lucide-react';
 
 const ModeToggle = () => {
   const [mounted, setMounted] = useState(false);
-  const {theme, setTheme} = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true);    
+    setMounted(true);
   }, []);
 
   if (!mounted) {
@@ -21,14 +28,8 @@ const ModeToggle = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className="focus-visible:ring-0 focus-visible:ring-offset-0">
-          {theme === 'system' ? (
-            <SunMoon />
-          ) : theme === 'dark' ? (
-            <MoonIcon />
-          ) : (
-            <SunIcon />
-          )}
+        <Button variant="ghost" className="focus-visible:ring-0 focus-visible:ring-offset-0">
+          {theme === 'system' ? <SunMoon /> : theme === 'dark' ? <MoonIcon /> : <SunIcon />}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
@@ -45,7 +46,7 @@ const ModeToggle = () => {
         </DropdownMenuCheckboxItem>
       </DropdownMenuContent>
     </DropdownMenu>
-   );
-}
- 
+  );
+};
+
 export default ModeToggle;
