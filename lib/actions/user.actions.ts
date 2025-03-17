0bd4e01bf67ff0902a1648d3e8c3c67f85fedd27
Seedling <david.r.graham32@gmail.com>
2025-03-17 11:23:06 -0400

    Implement query filter for users page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -16,6 +16,7 @@ import { ShippingAddress } from '@/types';
 import { z } from 'zod';
 import { PAGE_SIZE } from '../constants';
 import { revalidatePath } from 'next/cache';
+import { Prisma } from '@prisma/client';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -172,8 +173,29 @@ export async function updateProfile(user: { name: string; email: string }) {
 }
 
 // Get all users
-export async function getAllUsers({ limit = PAGE_SIZE, page }: { limit?: number; page: number }) {
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+  query,
+}: {
+  limit?: number;
+  page: number;
+  query: string;
+}) {
+  const queryFilter: Prisma.UserWhereInput =
+    query && query !== 'all'
+      ? {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.user.findMany({
+    where: {
+      ...queryFilter,
+    },
     orderBy: { createdAt: 'desc' },
     take: limit,
     skip: (page - 1) * limit,
